c7e44a3bc3d19535b2bd2c1b8c9edd2bb4c88bb6
Nana Anthony Appiah <nanaa@example.com>
2026-08-15 00:32:22 +0100

    Clean imports: fs, path, fileURLToPath

--- server.js
+++ server.js
@@ -1829,9 +1829,6 @@ Rules:
   }
 });
 
-import path from "path";
-import { fileURLToPath } from "url";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
